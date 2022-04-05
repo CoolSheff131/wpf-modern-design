0dde5cbd6fa895780bcc2ebf87718428dc5b4cc8
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-05 13:00:40 +0800

    HomeViewModel

--- ModernDesign/MVVM/ViewModel/MainViewModel.cs
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using ModernDesign.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModernDesign.MVVM.ViewModel
 {
 	class MainViewModel : ObservableObject
 	{
+		public HomeViewModel HomeVM { get; set; }
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value; 
+				OnPropertyChanged();
+			}
+		}
 		public MainViewModel()
 		{
-
+			HomeVM = new HomeViewModel();
+			CurrentView = HomeVM;
 		}
 	}
 }
